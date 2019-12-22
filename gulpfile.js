9d15fa4147ba1da3baaee8a5ddfb06f567f06772
Даниил Г <dankrsk@icloud.com>
2019-12-22 21:46:58 +0700

    Добавляет автоматизированную минификацию HTML и JS

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var del = require("del");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
+var pipeline = require("readable-stream").pipeline;
 var server = require("browser-sync").create();
 
 gulp.task("clean", function () {
@@ -20,14 +23,41 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/js/**",
     "source/*.ico",
-    "source/img/sprite.svg",
-    "source/*.html"
+    "source/img/sprite.svg"
   ], {
     base: "source"
   })
   .pipe(gulp.dest("build"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("js", function () {
+  return pipeline(
+    gulp.src("source/js/**/*.js", { base: "source" }),
+    uglify(),
+    gulp.dest("build")
+  );
 });
 
 gulp.task("images", function () {
@@ -46,27 +76,14 @@ gulp.task("webp", function () {
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
-});
-
 gulp.task("build", gulp.series(
   "clean",
   "copy",
+  "html",
+  "css",
+  "js",
   "images",
-  "webp",
-  "css"
+  "webp"
 ));
 
 gulp.task("server", function () {
@@ -79,12 +96,10 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html", gulp.series("refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
-  var htmlCopy = gulp.src("source/*.html")
-  .pipe(gulp.dest("build"));
   server.reload();
   done();
 });
